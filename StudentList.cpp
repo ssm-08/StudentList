33d5ab7114513055756165359efe3ce106e9a233
ssm-08 <shreesainarayanm@gmail.com>
2025-09-18 11:55:09 -0700

    set up variables

---
+++ StudentList.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstring>
+#include <vector>
+
+using namespace std;
+
+struct student {
+  char firstName[81];
+  char lastName[81];
+  int id;
+  float gpa;
+};
+
+int main() {
+  
+  vector<student*> studentList;
+
+  return 0;
+  
+}
