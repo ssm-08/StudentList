5c87b402209e4775da34b21374ea8fcb5bab3da2
ssm-08 <shreesainarayanm@gmail.com>
2025-09-23 11:26:04 -0700

    Added user input flow

--- StudentList.cpp
+++ StudentList.cpp
@@ -11,10 +11,36 @@ struct student {
   float gpa;
 };
 
+void addList(vector<student*> studentList);
+void printList(vector<student*> studentList);
+void deleteList(vector<student*> studentList);
+
 int main() {
   
   vector<student*> studentList;
+  char command[81];
 
+  char ADD[] = "ADD";
+  char PRINT[] = "PRINT";
+  char DELETE[] = "DELETE";
+  char QUIT[] = "QUIT";
+  
+  while (true) {
+  
+    cout << "Enter a command: ";
+    cin >> command;
+    
+    if (strcmp(command, ADD) == 0) {
+
+    } else if (strcmp(command, PRINT) == 0) {
+
+    } else if (strcmp(command, DELETE) == 0) {
+
+    } else if (strcmp(command, QUIT) == 0) {
+      exit(0);
+    }
+  }
+  
   return 0;
   
 }
